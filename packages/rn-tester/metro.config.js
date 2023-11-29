5d2adbc33174367f18be238df89e71e6802ce5df
Michał Pierzchała <thymikee@gmail.com>
2023-11-29 13:11:39 +0100

    create out-of-tree-platforms package and bring back resolver options

--- packages/rn-tester/metro.config.js
+++ packages/rn-tester/metro.config.js
@@ -14,25 +14,6 @@ const {getDefaultConfig} = require('@react-native/metro-config');
 const {mergeConfig} = require('metro-config');
 const path = require('path');
 
-// todo: extract to package
-const PlatformResolver = (config = {}) => {
-  const platformName =
-    config.platformName ?? '@callstack/react-native-visionos';
-
-  return (context, moduleName, platform) => {
-    let modifiedModuleName = moduleName;
-    if (moduleName === 'react-native') {
-      modifiedModuleName = platformName;
-    } else if (moduleName.startsWith('react-native/')) {
-      modifiedModuleName = `${platformName}/${modifiedModuleName.slice(
-        'react-native/'.length,
-      )}`;
-    }
-
-    return context.resolveRequest(context, modifiedModuleName, platform);
-  };
-};
-
 /**
  * This cli config is needed for development purposes, e.g. for running
  * integration tests during local development or on CI services.
@@ -57,8 +38,11 @@ const config = {
       'react-native': path.resolve(__dirname, '../react-native'),
     },
     resolveRequest: getPlatformResolver({
-      platformNameMap: {visionos: '@callstack/react-native-visionos'},
+      platformNameMap: {visionos: '@callstack/react-native-vision'},
     }),
+    sourceExts: getDefaultConfig(__dirname).resolver.sourceExts?.flatMap(
+      ext => [`visionos.${ext}`, ext],
+    ),
   },
 };
 
