fdedbd15edfb2faff916b85019b3e4902552a11c
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-12-14 13:06:48 +0100

    feat: implement visionOS CI (#54)
    feat: use Xcode beta globally
    
    feat: use callstack/hermes-visionos fork
    
    feat: test Hermes
    
    feat: reset cache keys
    
    fix: use callstack/hermes
    
    fix: use correct hermes source tarball
    
    fix: use never xcode version
    
    test only JSC
    
    fix: change Xcode version
    
    fix: use m1 executor
    
    feat: add cocoapods cache
    
    fix: remove hermes from cache keys
    
    fix: rbenv reset cache
    
    test: use 15.2
    
    fix: run visionos CI on every PR (#62)
    
    chore: disable some of upstream pipelines (#33)
    
    * chore: disable some of upstream pipelines
    
    * tests: update snapshot for visionOS
    
    * chore: disable more jobs, fix isVisionOS
    
    * fix: Keyboard.js, adjust pipeline_selection.js
    
    * feat: run iOS tests every night
    
    chore: disable android tests (#39)
    
    fix: prevent from running the testAll.yml on main (#41)
    
    fix: ci on main (#44)
    
    fix: remove windows jobs (#126)
    
    * fix: remove windows jobs
    
    * fix: sync jobs.yml

--- scripts/circleci/pipeline_selection.js
+++ scripts/circleci/pipeline_selection.js
@@ -155,6 +155,24 @@ function _computeAndSavePipelineParameters(
     return;
   }
 
+  // Custom config for visionOS
+  if (pipelineType === 'VISION_OS') {
+    const params = {
+      run_all: false,
+      run_ios: false,
+      run_visionos: true,
+      run_android: false,
+      run_js: true,
+      run_e2e: false,
+    };
+
+    const stringifiedParams = JSON.stringify(params, null, 2);
+    fs.writeFileSync(filePath, stringifiedParams);
+    console.info(`Generated params:\n${stringifiedParams}`);
+
+    return;
+  }
+
   console.log(`Should run e2e? ${shouldRunE2E}`);
   if (pipelineType === 'ALL') {
     fs.writeFileSync(
@@ -167,6 +185,7 @@ function _computeAndSavePipelineParameters(
   const params = {
     run_all: false,
     run_ios: pipelineType === 'RUN_IOS',
+    run_visionos: true,
     run_android: pipelineType === 'RUN_ANDROID',
     run_js: pipelineType === 'RUN_JS',
     run_e2e: shouldRunE2E,
@@ -194,6 +213,7 @@ function createConfigs(inputPath, outputPath, configFile) {
   const baseFolder = 'test_workflows';
   const testConfigs = {
     run_ios: ['testIOS.yml'],
+    run_visionos: ['testVisionOS.yml'],
     run_android: ['testAndroid.yml'],
     run_e2e: ['testE2E.yml'],
     run_all: ['testJS.yml', 'testAll.yml'],
@@ -243,5 +263,5 @@ function filterJobs(outputPath) {
       return;
     }
   }
-  _computeAndSavePipelineParameters('ALL', outputPath, shouldRunE2E);
+  _computeAndSavePipelineParameters('VISION_OS', outputPath, shouldRunE2E);
 }
