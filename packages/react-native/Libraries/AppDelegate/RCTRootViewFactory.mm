f26c0a56c6e1c342d2087bdbf0ed3e70c1eaf101
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-02-26 12:13:58 +0100

    feat: add multi-window support (#117)
    * feat: add multi-window support
    
    * feat: introduce WindowManager
    
    fix: RCTReactViewController properly check props to update
    
    fix: use clearColor instead of systemBackgroundColor for visionOS (#125)

--- packages/react-native/Libraries/AppDelegate/RCTRootViewFactory.mm
+++ packages/react-native/Libraries/AppDelegate/RCTRootViewFactory.mm
@@ -149,7 +149,11 @@ static NSDictionary *updateInitialProps(NSDictionary *initialProps, BOOL isFabri
         initWithSurface:surface
         sizeMeasureMode:RCTSurfaceSizeMeasureModeWidthExact | RCTSurfaceSizeMeasureModeHeightExact];
 
+#if TARGET_OS_VISION
+    surfaceHostingProxyRootView.backgroundColor = [UIColor clearColor];
+#else
     surfaceHostingProxyRootView.backgroundColor = [UIColor systemBackgroundColor];
+#endif
     return surfaceHostingProxyRootView;
   }
 
@@ -175,7 +179,11 @@ static NSDictionary *updateInitialProps(NSDictionary *initialProps, BOOL isFabri
   BOOL enableFabric = self->_configuration.fabricEnabled;
   UIView *rootView = RCTAppSetupDefaultRootView(bridge, moduleName, initProps, enableFabric);
 
+#if TARGET_OS_VISION
+  rootView.backgroundColor = [UIColor clearColor];
+#else
   rootView.backgroundColor = [UIColor systemBackgroundColor];
+#endif
 
   return rootView;
 }
