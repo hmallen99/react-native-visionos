0fcab8a7ba89836913e49115d087485310ce0e9a
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-03-25 15:45:10 +0100

    fix: local commands parameters (#134)

--- packages/react-native/local-cli/localCommands.js
+++ packages/react-native/local-cli/localCommands.js
@@ -1,43 +1,45 @@
 const {
-  buildOptions,
   createBuild,
   createLog,
   createRun,
-  logOptions,
-  runOptions,
+  getRunOptions,
+  getLogOptions,
+  getBuildOptions,
 } = require('@react-native-community/cli-platform-apple');
+
+const platformName = 'visionos';
 
 const run = {
   name: 'run-visionos',
   description: 'builds your app and starts it on visionOS simulator',
-  func: createRun({platformName: 'visionos'}),
+  func: createRun({platformName}),
   examples: [
     {
       desc: 'Run on a specific simulator',
       cmd: 'npx @callstack/react-native-visionos run-visionos --simulator "Apple Vision Pro"',
     },
   ],
-  options: runOptions,
+  options: getRunOptions({platformName}),
 };
 
 const log = {
   name: 'log-visionos',
   description: 'starts visionOS device syslog tail',
   func: createLog({platformName: 'visionos'}),
-  options: logOptions,
+  options: getLogOptions({platformName}),
 };
 
 const build = {
   name: 'build-visionos',
   description: 'builds your app for visionOS platform',
-  func: createBuild({platformName: 'visionos'}),
+  func: createBuild({platformName}),
   examples: [
       {
       desc: 'Build the app for all visionOS devices in Release mode',
       cmd: 'npx @callstack/react-native-visionos build-visionos --mode "Release"',
       },
   ],
-  options: buildOptions,
+  options: getBuildOptions({platformName}),
 };
 
 module.exports = [run, log, build];
