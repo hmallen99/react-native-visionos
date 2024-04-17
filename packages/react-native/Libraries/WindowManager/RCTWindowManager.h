899594bf83ca290265ba446e01df3b8681321f92
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-04-17 13:11:50 +0200

    feat: implement window manager events (#142)

--- packages/react-native/Libraries/WindowManager/RCTWindowManager.h
+++ packages/react-native/Libraries/WindowManager/RCTWindowManager.h
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RCTWindowManager : NSObject <RCTBridgeModule>
+@interface RCTWindowManager : RCTEventEmitter
 
 @end
